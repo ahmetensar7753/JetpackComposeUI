dfbddf9fa4fa6f03a028907297fe5b187987f409
ahmetensar7753 <ensarbesir7753@gmail.com>
2023-12-03 18:58:53 +0300

    Files installed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# JetpackComposeUI
+# Kotlin Jetpack Compose UI Elements
+
+## In this project, the UI elements of the Kotlin Jetpack Compose library have been explored.
---
+++ app/src/main/java/com/example/layoutpart/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.layoutpart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.layoutpart.ui.theme.LayoutPartTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LayoutPartTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LayoutPartTheme {
+        Greeting("Android")
+    }
+}
